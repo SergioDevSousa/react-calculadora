fc6666e81492d795c4154bde40e0c4e100d90a25
SergioDevSousa <oxentesergiosousa76@outlook.com>
2025-01-13 15:57:28 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import Input from './components/Input';
+import Button from './components/Buttons';
+import { Container, Content, Row, Column } from './styles';
+
+function App() {
+  return (
+    <Container >
+      <Content>
+        <Input />
+        <Row>
+          <Button label="7"/>
+          <Button label="8"/>
+          <Button label="9"/>
+          <Button label="-"/>
+        </Row>
+        <Row>
+          <Button label="4"/>
+          <Button label="5"/>
+          <Button label="6"/>
+          <Button label="+"/>
+        </Row>
+        <Row>
+          <Button label="1"/>
+          <Button label="2"/>
+          <Button label="3"/>
+          <Button label="="/>
+        </Row>
+      </Content>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/Buttons/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    flex: 1;
+    padding: 10px;
+    border: 1px solid #cdcdcd;
+    background-color: #0000ff;
+    color: #ffffff;
+    font-size:  24px;
+    font-weight: 700;
+
+`
